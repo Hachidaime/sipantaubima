e3c376e3e820b38c2c3948f0eaa4e244ff2e53f9
Hachidaime <cold.kid40@gmail.com>
2020-10-17 17:20:25 +0700

    Update Dashboard: fix invalid array

--- app/models/DashboardModel.php
+++ app/models/DashboardModel.php
@@ -45,29 +45,31 @@ class DashboardModel extends Model
         $activityInfo = array_values($activityOpt);
 
         foreach ($activityInfo as $idx => $row) {
-            $row['all'] = count($row['detail']);
-
             $red = 0;
             $yellow = 0;
             $green = 0;
             $finish = 0;
 
-            foreach ($row['detail'] as $key => $value) {
-                switch ($value['indicator']) {
-                    case 'red':
-                        $red += 1;
-                        break;
+            if (is_array($row['detail'])) {
+                $row['all'] = count($row['detail']);
 
-                    case 'yellow':
-                        $yellow += 1;
-                        break;
+                foreach ($row['detail'] as $key => $value) {
+                    switch ($value['indicator']) {
+                        case 'red':
+                            $red += 1;
+                            break;
 
-                    case 'green':
-                        $green += 1;
-                        break;
-                }
-                if ($value['prog_physical'] == 100) {
-                    $finish += 1;
+                        case 'yellow':
+                            $yellow += 1;
+                            break;
+
+                        case 'green':
+                            $green += 1;
+                            break;
+                    }
+                    if ($value['prog_physical'] == 100) {
+                        $finish += 1;
+                    }
                 }
             }
 
@@ -79,59 +81,6 @@ class DashboardModel extends Model
             $activityInfo[$idx] = $row;
         }
 
-        // print '<pre>';
-        // // print_r($performance);
-        // print_r($activityInfo);
-        // print '</pre>';
-        /* foreach ($list as $idx => $row) {
-            $red = 0;
-            $yellow = 0;
-            $green = 0;
-            $white = 0;
-            $finish = 0;
-
-            foreach ($row['detail'] as $i => $r) {
-                foreach ($r as $k => $v) {
-                    if (!in_array($k, ['indicator'])) {
-                        unset($r[$k]);
-                    }
-                    $row['detail'][$i] = $r;
-                }
-
-                switch ($r['indicator']) {
-                    case 'red':
-                        $red++;
-                        break;
-
-                    case 'yellow':
-                        $yellow++;
-                        break;
-
-                    case 'green':
-                        $green++;
-                        break;
-
-                    default:
-                        $white++;
-                        break;
-                }
-            }
-
-            $row['detail_count'] = count($row['detail']);
-
-            // var_dump($row);
-            // foreach ($row as $key => $value) {
-            //     if (!in_array($key, ['prg_name', 'act_name'])) {
-            //         unset($row[$key]);
-            //     }
-            // }
-            $row['indicator_red'] = $red;
-            $row['indicator_yellow'] = $yellow;
-            $row['indicator_green'] = $green;
-            $row['indicator_white'] = $white;
-
-            $list[$idx] = $row;
-        } */
         return $activityInfo;
     }
 }
