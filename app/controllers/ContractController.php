7abae19551306dbb5d89641e39b805dcdc500e25
Hachidaime <cold.kid40@gmail.com>
2020-12-01 09:33:35 +0700

    Update Contract: validate Nilai Addendum

--- app/controllers/ContractController.php
+++ app/controllers/ContractController.php
@@ -111,7 +111,9 @@ class ContractController extends Controller
         $addendum = [];
         foreach ($data as $key => $value) {
             if (is_array($value)) {
-                $addendum[$key] = $value;
+                foreach ($value as $k => $val) {
+                    $addendum[$k][$key] = $val;
+                }
                 unset($data[$key]);
             } else {
                 if (empty($value)) {
@@ -120,7 +122,7 @@ class ContractController extends Controller
             }
         }
 
-        if ($this->validate($data)) {
+        if ($this->validate($data, $addendum)) {
             $result = $this->contractModel->save($data);
 
             $tag = 'Ubah';
@@ -146,7 +148,7 @@ class ContractController extends Controller
         }
     }
 
-    public function validate($data)
+    public function validate($data, $addendum)
     {
         $validation = $this->validator->make($data, [
             'cnt_contractor_name' => 'required|max:250',
@@ -184,35 +186,33 @@ class ContractController extends Controller
                 'msg' => $validation->errors()->firstOfAll()
             ]);
             exit();
+        } else {
+            $this->validateAddendum($data, $addendum);
         }
         return true;
     }
 
     private function submitAddendum($data)
     {
-        for ($i = 1; $i <= 8; $i++) {
+        foreach ($data as $row) {
             $addendum = [
                 'pkgd_id' => $_POST['pkgd_id'],
-                'id' => $data['add_id'][$i],
-                'add_no' => strtoupper($data['add_no'][$i]),
-                'add_order' => $data['add_order'][$i],
-                'add_date' => !empty($data['add_date'][$i])
+                'id' => $row['add_id'],
+                'add_no' => strtoupper($row['add_no']),
+                'add_order' => $row['add_order'],
+                'add_date' => !empty($row['add_date'])
+                    ? Functions::dateFormat('d/m/Y', 'Y-m-d', $row['add_date'])
+                    : '',
+                'add_days' => $row['add_days'],
+                'add_plan_pho_date' => !empty($row['add_plan_pho_date'])
                     ? Functions::dateFormat(
                         'd/m/Y',
                         'Y-m-d',
-                        $data['add_date'][$i]
+                        $row['add_plan_pho_date']
                     )
                     : '',
-                'add_days' => $data['add_days'][$i],
-                'add_plan_pho_date' => !empty($data['add_plan_pho_date'][$i])
-                    ? Functions::dateFormat(
-                        'd/m/Y',
-                        'Y-m-d',
-                        $data['add_plan_pho_date'][$i]
-                    )
-                    : '',
-                'add_value' => !empty($data['add_value'][$i])
-                    ? str_replace(',', '.', $data['add_value'][$i])
+                'add_value' => !empty($row['add_value'])
+                    ? str_replace(',', '.', $row['add_value'])
                     : ''
             ];
 
@@ -223,6 +223,34 @@ class ContractController extends Controller
             }
 
             $this->addendumModel->save($addendum);
+        }
+    }
+
+    private function validateAddendum($data, $addendum)
+    {
+        $errors = [];
+        foreach ($addendum as $idx => $row) {
+            foreach ($row as $key => $value) {
+                $value =
+                    $key == 'add_value'
+                        ? str_replace(',', '.', $value)
+                        : $value;
+                $row[$key . $idx] = $value;
+                unset($row[$key]);
+            }
+
+            if ($row["add_value{$idx}"] > $data['cnt_value']) {
+                $errors["add_value{$idx}"] =
+                    '<strong>Nilai Addendum</strong> tidak boleh lebih dari <strong>Nilai Kontrak</strong>.';
+            }
+        }
+
+        if ($errors) {
+            echo json_encode([
+                'success' => false,
+                'msg' => $errors
+            ]);
+            exit();
         }
     }
 }
